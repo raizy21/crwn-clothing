0b2f133b3f86390b1323e6855f9a435cd84585c5
raizy <chiper_raizy@yahoo.com>
2021-04-19 09:21:51 +0200

    creating a new sign in and sign up page, as well sign in component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Switch,Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 function App() {
   return ( 
@@ -14,9 +15,12 @@ function App() {
     <Switch>
         <Route exact path='/' component={HomePage} />
         <Route  path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
+
+
 }
 
 export default App;
