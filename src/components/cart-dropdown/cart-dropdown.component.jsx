de3a3a2693c0f143b593fe051e4ff96096ba1324
raizy <chiper_raizy@yahoo.com>
2021-04-25 11:10:59 +0200

    adding redux-persist to persist cart reducer accross session

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import {withRouter} from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
-import {toggleCartHidden} from '../../redux/cart/cart.actions.js';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
 import './cart-dropdown.styles.scss';
 
@@ -15,20 +15,19 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-items'>
       {cartItems.length ? (
         cartItems.map(cartItem => (
-          <CartItem key={cartItem.id} item ={cartItem} />
-        )) 
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
       ) : (
-        <span className='empty-message'>your cart is empty</span>
-      )
-    }
-
+        <span className='empty-message'>Your cart is empty</span>
+      )}
     </div>
     <CustomButton
       onClick={() => {
         history.push('/checkout');
-        dispatch(toggleCartHidden()); 
-        }}>
-         go to checkout
+        dispatch(toggleCartHidden());
+      }}
+    >
+      GO TO CHECKOUT
     </CustomButton>
   </div>
 );
